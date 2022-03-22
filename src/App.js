a81a53405c2a4df9fab9b865faa1e884fad32e2f
CalDevC <mr.alexander5132@gmail.com>
2022-03-22 11:34:55 -0700

    Basic frontend can send files to backend on Goolge Cloud VM

--- src/App.js
+++ src/App.js
@@ -26,16 +26,26 @@ class App extends React.Component {
 
   sendFile() {
     console.log("Sending...");
+    fetch("/upload")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("DATA: " + data.message);
+        return this.setState({ data: data.message })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render() {
     return (
       <div className="App" >
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <form method="POST" action="/upload" encType="multipart/form-data">
-            <label htmlFor="uploadFile">Choose an audio file to upload</label>
-            <input type="file" name="uploadFile" ></input>
+          <form method="POST" action="/upload" encType="multipart/form-data" >
+            <label htmlFor="uploadedFile">Choose an audio file to upload</label>
+            <br />
+            <input type="file" name="uploadedFile" ></input>
+            <br />
             <button type="submit" name="submit">Send file</button>
           </form>
           <p>{this.state.data}</p>
