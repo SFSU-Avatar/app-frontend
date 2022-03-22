360d3a682ad2b54086f7f6fd686148fbcdad34ea
CalDevC <mr.alexander5132@gmail.com>
2022-03-22 11:14:04 -0700

    Converted App component to class and added file upload

--- src/App.js
+++ src/App.js
@@ -4,26 +4,46 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  const [data, setData] = React.useState(null);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: ""
+    }
+  }
 
-  React.useEffect(() => {
+  componentDidMount() {
     fetch("/api")
-      .then((res) => {
-        console.log("DATA: " + data);
-        return res.json()
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("DATA: " + data.message);
+        return this.setState({ data: data.message })
       })
-      .then((data) => setData(data.message));
-  }, []);
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
-  );
+  sendFile() {
+    console.log("Sending...");
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <form method="POST" action="/upload" encType="multipart/form-data">
+            <label htmlFor="uploadFile">Choose an audio file to upload</label>
+            <input type="file" name="uploadFile" ></input>
+            <button type="submit" name="submit">Send file</button>
+          </form>
+          <p>{this.state.data}</p>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
